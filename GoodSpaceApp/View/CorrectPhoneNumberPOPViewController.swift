53f7fe596593276d3b00bbc8fdebae4eca986c2f
bharat <bharatsilavat96@gmail.com>
2023-12-05 04:38:02 +0530

    Added All Screen UI part from dashboard

---
+++ GoodSpaceApp/View/CorrectPhoneNumberPOPViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CorrectPhoneNumberPOPViewController.swift
+//  GoodSpaceApp
+//
+//  Created by Bharat Shilavat on 05/12/23.
+//
+
+import UIKit
+
+class CorrectPhoneNumberPOPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+}
---
+++ GoodSpaceApp/View/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInScreenViewController.swift
+//  GoodSpaceApp
+//
+//  Created by Bharat Shilavat on 05/12/23.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
